dd29ea3f53f2db73bc5828477e09a36e952034a0
zkl <you@example.com>
2020-08-30 18:59:05 +0800

    实现搜索全球城市数据功能

---
+++ app/src/main/java/com/sunnyweather/android/logic/network/ServiceCreator.kt
@@ -0,0 +1,19 @@
+package com.sunnyweather.android.logic.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ServiceCreator {
+
+    private const val BASE_URL = "https://api.caiyunapp.com/"
+
+    private val retofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    fun <T> create(serviceClass: Class<T>) : T = retofit.create(serviceClass)
+
+    inline fun <reified  T> create() : T = create(T::class.java)
+
+}
